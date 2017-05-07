7f78f49dc53a7a97f725a2d13ee335bdaa255564
deepakqa13 <deepak.qa13@gmail.com>
2017-05-07 16:23:14 +0530

    Initial commit

---
+++ src/main/java/com/SeleniumAnyWhere/com/anyAut/App.java
@@ -0,0 +1,13 @@
+package com.SeleniumAnyWhere.com.anyAut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/SeleniumAnyWhere/com/anyAut/LoginTest.java
@@ -0,0 +1,32 @@
+package com.SeleniumAnyWhere.com.anyAut;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	@BeforeMethod
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	@Test
+	public void doLogin(){
+		driver.get("https://www.gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("Password")).sendKeys("abc@gmail.com");
+		
+	}
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+}
