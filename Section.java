a19381e47bdf60f9dfcdfa88426afad6e6f27c50
deepakqa13 <deepak.qa13@gmail.com>
2017-05-07 17:17:51 +0530

    Add files via upload

---
+++ Section.java
@@ -0,0 +1,18 @@
+package Constructor;
+
+public class Section {
+
+	public static void main(String[] args) {
+		Student s1= new Student(1, "Anil");		
+		Student s2= new Student(2, "Deepak",34);	
+		
+		
+		s1.displayRecords();
+			
+		s2.displayRecords();
+		 
+				
+
+	}
+
+}
